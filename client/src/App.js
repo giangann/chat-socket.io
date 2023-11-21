8fd8a054a31a874e53a535822e246f7454c0ce96
giangann <70595717+giangann@users.noreply.github.com>
2023-11-22 00:39:07 +0700

    basic send message, broadcast

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState, useRef} from 'react'
+import {io} from 'socket.io-client'
 function App() {
+
+  const inputRef = useRef(null)
+  const socket = io('http://localhost:5000')
+  const handleSend = () =>{
+    socket.emit('send-msg', inputRef.current.value)
+  }
+
+  socket.on('reply-msg',(message)=>{
+    console.log(message)
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.  
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={inputRef}/>
+      <button onClick={handleSend}>send</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-const socket = require("socket.io");
-require("dotenv").config();
+const socket = require('socket.io') 
+const cors = require('cors')
+require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
@@ -14,3 +14,18 @@ app.get("/", (req, res) => {
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on ${process.env.PORT}`);
 });
+
+const io = socket (server,{
+  cors:{
+    origin: 'http://localhost:3000',
+    cridentals:true
+  }
+})
+
+io.on('connection',(socket)=>{
+  console.log('have someone connect!')
+  socket.on('send-msg',(message)=>{
+    console.log(message)
+    socket.broadcast.emit('reply-msg',message)
+  })
+})
